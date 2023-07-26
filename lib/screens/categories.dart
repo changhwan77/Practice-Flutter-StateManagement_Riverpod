b2a743109ce22186dfb72caf0b7ed4728e685e0b
changhwan77 <ckdghks9741@gmail.com>
2023-07-26 16:21:13 +0900

    feat: add main_layout
    첫 커밋 & GridView를 이용한 main 화면 layout 생성

---
+++ lib/screens/categories.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick Your Category'),
+      ),
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              childAspectRatio: 3 / 2),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(category: category),
+          ]),
+    );
+  }
+}
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({Key? key, required this.category}) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      borderRadius: BorderRadius.circular(16),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
